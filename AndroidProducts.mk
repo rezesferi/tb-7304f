5dfe40859d722338c450abf014edc9cbb3dd2e08
rezesferi <126973486+rezesferi@users.noreply.github.com>
2024-08-23 16:29:57 +0200

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TB-7304F.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TB-7304F-user \
+    omni_TB-7304F-userdebug \
+    omni_TB-7304F-eng
